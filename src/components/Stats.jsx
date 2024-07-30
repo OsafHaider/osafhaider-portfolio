f8e2414be077be833905fbcfb017da4a5230c239
OsafTheDeveloper <osafh486@gmail.com>
2024-07-30 22:43:45 +0500

    fix the code

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,17 +6,17 @@ const Stats = () => {
   return (
     <section className=" pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+        <div  className="grid grid-cols-2 gap-6 max-w-[80vw] mx-auto xl:max-w-none lg:grid-cols-4">
           {stats.map((stat, index) => (
             <div
-              className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+              className="flex-1 flex gap-2 items-center justify-center xl:justify-start"
               key={index}
             >
               <CountUp
                 end={stat.num}
                 duration={5}
                 delay={2}
-                className="text-4xl xl:text-6xl font-extrabold"
+                className="text-3xl xl:text-6xl font-extrabold"
               />
               <p
                 className={`${
